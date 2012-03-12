6cd4ea6963b73eff9576b11a9b6e012fd60a61b9
Frizlab <frizlab@frizlab.dyndns.org.home>
2012-03-12 08:50:28 +0100

    Initial Commit

---
+++ Volume Name Icon Reader/FLDocument.h
@@ -0,0 +1,13 @@
+//
+//  FLDocument.h
+//  Volume Name Icon Reader
+//
+//  Created by François LAMBOLEY on 3/12/12.
+//  Copyright (c) 2012 Frost Land. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FLDocument : NSDocument
+
+@end
---
+++ Volume Name Icon Reader/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Volume Name Icon Reader
+//
+//  Created by François LAMBOLEY on 3/12/12.
+//  Copyright (c) 2012 Frost Land. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
