f37eb3128381bbbd7a5dbc2ced9583fac995dc98
Kevin Wojniak <kainjow@kainjow.com>
2014-09-27 09:12:06 -0700

    Remove unimplemented save code.

--- Volume Name Icon Reader/FLDocument.m
+++ Volume Name Icon Reader/FLDocument.m
@@ -30,20 +30,6 @@
 	[self updateUI];
 }
 
-+ (BOOL)autosavesInPlace
-{
-	return YES;
-}
-
-- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
-{
-	// Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-	// You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-	NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-	@throw exception;
-	return nil;
-}
-
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
 {
 	if ([data length] < 5) {
